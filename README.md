c76c2d8cdd8b86af0378ad0cfc94fe677c6d55cd
Ankur <ankurchanda198@gmail.com>
2024-03-09 20:38:32 +0530

    Interface

---
+++ README.md
@@ -0,0 +1,6 @@
+---
+title: Programming_Code_Assistant_App
+app_file: app.py
+sdk: gradio
+sdk_version: 4.21.0
+---
--- app.py
+++ app.py
@@ -2,27 +2,39 @@ import requests
 import json
 import gradio as gr
 
-url="http://localhost:11434/api/generate"
+# https://github.com/ollama/ollama
+
+url="http://localhost:11434/api/generate" #api
 
 headers={
 
-    'Content-Type':'application/json'
+    'Content-Type':'application/json' #api documentation for messages
 }
 
-history=[]
+history=[] #save prev info
 def generate_response(prompt):
     history.append(prompt)
-    final_prompt= "\n".join(history)
+    final_prompt= "\n".join(history) #append history
     data= {
         "model": "Jarvis",
         "prompt": final_prompt,
-        "stream": False
+        "stream": False    #remove unnecessary values
     }
     response = requests.post(url,headers=headers, data=json.dumps(data))
-    if response.status_code==200:
+    if response.status_code==200: #successful
         response=response.text
         data=json.loads(response)
         actual_response=data['response']
         return actual_response
     else:
         print("error:",response.text)
+
+
+# Front-end
+interface=gr.Interface(
+    title="Programming Code Assistant App",
+    fn=generate_response,
+    inputs=gr.Textbox(lines=4,placeholder="Enter your Prompt"),
+    outputs="text"
+)
+interface.launch(share=True)
